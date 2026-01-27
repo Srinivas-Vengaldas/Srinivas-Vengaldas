f39b14a208b6fcb150239b2821de9a66896f6923
Srinivas Vengaldas <89868520+Srinivas-Vengaldas@users.noreply.github.com>
2026-01-27 04:41:45 -0500

    Adjust paddingLeft based on isCompact state

--- Portfolio/my-portfolio/src/App.jsx
+++ Portfolio/my-portfolio/src/App.jsx
@@ -561,7 +561,7 @@ const HeroSection = () => {
     return (
     <section style={{ 
         height: '100vh', width: '100%', position: 'relative', display: 'flex', alignItems: 'center',
-        paddingLeft: isCompact ? '5%' : '8%', pointerEvents: 'none',
+        paddingLeft: isCompact ? '0' : '8%', pointerEvents: 'none',
         paddingRight: isCompact ? '0' : '0'
     }}>
         <div style={{ zIndex: 5, maxWidth: '650px', pointerEvents: 'auto', textAlign: isCompact ? 'center' : 'left', width: isCompact ? '100%' : 'auto' }}>
